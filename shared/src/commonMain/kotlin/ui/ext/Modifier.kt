afc039dc1b2f3cfd96ac641d54daf431e4c29acb
hackvem <part1shb@gmail.com>
2023-07-15 17:39:35 +0900

    add: noRippleClickable()
    - 클릭 영역 임의 제거

---
+++ shared/src/commonMain/kotlin/ui/ext/Modifier.kt
@@ -0,0 +1,26 @@
+package ui.ext
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+fun Modifier.noRippleClickable(
+    onPress: ((Boolean) -> Unit)? = null,
+    onClick: () -> Unit,
+): Modifier = composed {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    if (onPress != null) onPress(isPressed)
+
+    clickable(
+        indication = null,
+        interactionSource = interactionSource
+    ) {
+        onClick()
+    }
+}
