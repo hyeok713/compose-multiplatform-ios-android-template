7dcf23a68eb062fc0cbc138119e12f560e5afb82
hackvem <part1shb@gmail.com>
2023-07-15 17:40:28 +0900

    res: added some strings

--- shared/src/commonMain/kotlin/values/Strings.kt
+++ shared/src/commonMain/kotlin/values/Strings.kt
@@ -1,13 +1,22 @@
 package values
 
 import androidx.compose.runtime.Immutable
-
 @Immutable
 data class StringResources(
     val hello: String,
+    val game_roulette: String,
+    val enter_target: String,
+    val restart: String,
+    val start: String,
+    val info_roulette: String,
 )
 
 fun stringResourcesEn() = StringResources(
     hello = "hello",
+    game_roulette = "Roulette Game",
+    enter_target = "Enter target name",
+    restart = "RE-START",
+    start = "START!",
+    info_roulette = "You can manipulate game result.\n \'DOUBLE TAP\' to set target!"
 )
 
--- shared/src/commonMain/kotlin/values/StringsKo.kt
+++ shared/src/commonMain/kotlin/values/StringsKo.kt
@@ -2,4 +2,9 @@ package values
 
 fun stringResourcesKo() = StringResources(
     hello = "안녕",
+    game_roulette = "룰렛 돌리기",
+    enter_target = "타겟의 이름을 입력해 주세요",
+    restart = "다시 돌리기",
+    start = "시작!",
+    info_roulette = "특정 타겟을\'더블 탭\' 하여 \n게임 결과를 조작할 수 있습니다!"
 )
